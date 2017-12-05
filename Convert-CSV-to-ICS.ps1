471c04cbd1fcc300ae518824b5c0935688d6cb3c
maurizioarrigoni <maurizio.arrigoni@tiscali.it>
2017-12-05 07:09:44 +0100

    template.csv

--- Convert-CSV-to-ICS.ps1
+++ Convert-CSV-to-ICS.ps1
@@ -1,2 +1,6 @@
-﻿$OutputFilename = $PSScriptRoot + "\output.ics"
+﻿$InputFilename = $PSScriptRoot + "\template.csv"
+$OutputFilename = $PSScriptRoot + "\output.ics"
 echo $OutputFilename
+$CSV=Import-Csv $InputFilename
+echo $CSV[1]
+
