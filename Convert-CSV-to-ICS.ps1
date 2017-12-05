a57e065ac4964360495f9ecc589a2119c3307f69
maurizioarrigoni <maurizio.arrigoni@tiscali.it>
2017-12-05 19:29:24 +0100

    20171205

--- Convert-CSV-to-ICS.ps1
+++ Convert-CSV-to-ICS.ps1
@@ -1,13 +1,17 @@
 ﻿$InputFilename = $PSScriptRoot + "\template.csv"
 $OutputFilename = $PSScriptRoot + "\output.ics"
-echo $OutputFilename
+
 $CSV=Import-Csv $InputFilename
 
 # get CSV headers
 $Headers = $CSV | Get-Member -MemberType NoteProperty
 
-
-For ($i=0; $i -le $CSV.Count; $i++) {
-    echo $i
-    echo $CSV[$i]
+ForEach ($Line in $CSV) {
+    echo $Line
 }
+
+
+# For ($i=0; $i -le $CSV.Count; $i++) {
+#    echo $i
+#    echo $CSV[$i]
+#}
