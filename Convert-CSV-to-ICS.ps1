9b87122c89db01b59e7e96842fdab5b275affb20
Francesco Menin <francesco.menin@horsa.it>
2025-03-21 10:12:00 +0100

    Update Convert-CSV-to-ICS.ps1
    match CSV fields to ICS

--- Convert-CSV-to-ICS.ps1
+++ Convert-CSV-to-ICS.ps1
@@ -14,37 +14,16 @@ $Headers = $CSV | Get-member -MemberType 'NoteProperty' | Select-Object -ExpandP
 
 "BEGIN:VCALENDAR
 PRODID:
-VERSION:2.0
-CALSCALE:GREGORIAN
-METHOD:PUBLISH
-X-WR-CALNAME:TEST IMPORT
-X-WR-TIMEZONE:Europe/Rome
-BEGIN:VTIMEZONE
-TZID:Europe/Rome
-X-LIC-LOCATION:Europe/Rome
-BEGIN:DAYLIGHT
-TZOFFSETFROM:+0100
-TZOFFSETTO:+0200
-TZNAME:CEST
-DTSTART:19700329T020000
-RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU
-END:DAYLIGHT
-BEGIN:STANDARD
-TZOFFSETFROM:+0200
-TZOFFSETTO:+0100
-TZNAME:CET
-DTSTART:19701025T030000
-RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
-END:STANDARD
-END:VTIMEZONE" > $OutputFilename
+VERSION:2.0" > $OutputFilename
 
 ForEach ($Line in $CSV) {
     "BEGIN:VEVENT" >> $OutputFilename
-    ForEach ($FieldName in $Headers) {
-        $($($FieldName)+":"+$($Line.$FieldName)) >> $OutputFilename
-    }
-    "TRANSP:TRANSPARENT" >> $OutputFilename
-    "END:VEVENT" >> $OutputFilename
+    "SUMMARY:$($(Line.Subject)" >> $OutputFilename
+    "DTSTART:$($startDateTime.ToString('yyyyMMddTHHmmssZ'))" >> $OutputFilename
+    "nDTEND:$($endDateTime.ToString('yyyyMMddTHHmmssZ'))" >> $OutputFilename
+    "LOCATION:$($entry.Location)" >> $OutputFilename
+    "DESCRIPTION:$($entry.Description)" >> $OutputFilename
+    "END:VEVENT" >> $OutputFilename >> $OutputFilename
 }
 
 "END:VCALENDAR" >> $OutputFilename
